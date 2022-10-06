31466a4501844b416e441b0ea9ba8d25ee91f6a6
GilangAsh14 <91508048+GilangAsh14@users.noreply.github.com>
2022-10-06 23:29:01 +0700

    Add files via upload
    Tugas individu 1

---
+++ tugas1/src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Email: gilangaabimanyu@gmail.com <br />
+           Phone: 082211871064
+</p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
